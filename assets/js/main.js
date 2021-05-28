f66ff208f7ef78b482eddb7c9d45dd38e696e722
GasGas7 <79295810+GasGas7@users.noreply.github.com>
2021-05-28 13:06:32 +0200

    error vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,31 @@
+
+/* 
+Utilizzando vue, stampiamo a schermo una card per ogni album.
+BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+BONUS 2: Ordinare i dischi per anno di uscita. */
+
+
 const app = new Vue ({
 
     el:'#app',
 
     data: {
-
+        url:"https://flynn.boolean.careers/exercises/api/array/music",
+        titolo:"",
     },
     methods:{
         
     },
     mounted(){
-        
+        axios
+            .get(this.url)
+            .then(resp => {
+
+                this.titolo = resp.response;
+
+            }).catch(e => {
+                console.error(e)
+            })
     }
 
 })
